f64f37594bc6e3c11879f2a29e49369feba282c2
ClovenBugle <yan_hun233@outlook.com>
2026-01-31 01:07:42 +0800

    ver(0.3.3): bump plugin version to 0.3.3

--- src/main/java/top/ourisland/litemacro/BuildConstants.java
+++ src/main/java/top/ourisland/litemacro/BuildConstants.java
@@ -3,5 +3,5 @@ package top.ourisland.litemacro;
 // The constants are replaced before compilation
 public class BuildConstants {
 
-    public static final String VERSION = "0.3.2";
+    public static final String VERSION = "0.3.3";
 }
